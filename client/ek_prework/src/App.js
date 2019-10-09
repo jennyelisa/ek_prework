c158e13d2ef0dce44cce60689f133d0af837e2f5
Jenny <jennyelisa07@aol.com>
2019-10-09 11:32:10 -0400

    form is rendering, working on displaying values, then queries

--- client/ek_prework/src/App.js
+++ client/ek_prework/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import FormBox from "./Components/FormBox/form";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormBox />
     </div>
   );
 }
